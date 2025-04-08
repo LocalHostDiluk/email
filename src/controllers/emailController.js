cd785ff38b1abcffd69d719bdf8754417928c980
LocalHostDiluk <169072534+LocalHostDiluk@users.noreply.github.com>
2025-04-08 15:42:04 -0600

    Update emailController.js

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,30 +1,17 @@
 import transporter from "../config/emailConfig.js";
 import dotenv from "dotenv";
-import hbs from "nodemailer-express-handlebars";
-import path from "path";
 
 dotenv.config();
 
-const opcionesHandlebars = {
-  viewEngine: {
-    partialsDir: path.resolve("./views/partials"), // Si usas partials
-    defaultLayout: "./views/main.hbs", // Habilita el diseño principal
-  },
-  viewPath: path.resolve("./views/"),
-  extName: ".hbs",
-};
-
-transporter.use("compile", hbs(opcionesHandlebars));
-
 export const sendEmail = async (req, res) => {
-  const { to, subject, context, template } = req.body;
+  const { to, subject, nombre, mensaje } = req.body;
   try {
     await transporter.sendMail({
       from: process.env.EMAIL,
       to,
       subject,
-      template: template,
-      context: context,
+      template: 'email', // Nombre de la plantilla Handlebars
+      context: { nombre, mensaje },
     });
     return res.status(200).json({ message: "Correo enviado con éxito" });
   } catch (error) {
